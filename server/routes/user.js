b5fb3bc1d0c3fdc49b670bb654ff0422391ea3df
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-04-18 14:12:24 +1000

    Comment unused controllers.

--- server/routes/user.js
+++ server/routes/user.js
@@ -58,49 +58,49 @@ router.post("/logInWithEmailAndPassword", (req, res, next) => {
     });
 });
 
-router.get("/logInWith3rdParty", (req, res, next) => {
-  try {
-    const provider =
-      req.query.provider === "github"
-        ? new auth.GithubAuthProvider()
-        : new auth.GoogleAuthProvider();
-    auth()
-      .signInWithPopup(provider)
-      .then((user) => {
-        if (user) {
-          const isLoggedInWithEmailAndPw =
-            user.additionalUserInfo.providerId === "password";
-          const isEmailVerified = user.user.emailVerified;
+// router.get("/logInWith3rdParty", (req, res, next) => {
+//   try {
+//     const provider =
+//       req.query.provider === "github"
+//         ? new auth.GithubAuthProvider()
+//         : new auth.GoogleAuthProvider();
+//     auth()
+//       .signInWithPopup(provider)
+//       .then((user) => {
+//         if (user) {
+//           const isLoggedInWithEmailAndPw =
+//             user.additionalUserInfo.providerId === "password";
+//           const isEmailVerified = user.user.emailVerified;
 
-          !isEmailVerified && auth().currentUser.sendEmailVerification();
+//           !isEmailVerified && auth().currentUser.sendEmailVerification();
 
-          res.send({
-            userId: user.user.uid,
-            displayName: user.user.displayName,
-            email: user.user.email,
-            isLoggedInWithEmailAndPw,
-            isAllowedToUseChatroom:
-              !isLoggedInWithEmailAndPw ||
-              (isLoggedInWithEmailAndPw && isEmailVerified),
-          });
-        } else throw { message: "No user details found." };
-      })
-      .catch((error) => {
-        res.status(500).send(error.message || error);
-      });
-  } catch (error) {
-    console.error(error);
-  }
-});
+//           res.send({
+//             userId: user.user.uid,
+//             displayName: user.user.displayName,
+//             email: user.user.email,
+//             isLoggedInWithEmailAndPw,
+//             isAllowedToUseChatroom:
+//               !isLoggedInWithEmailAndPw ||
+//               (isLoggedInWithEmailAndPw && isEmailVerified),
+//           });
+//         } else throw { message: "No user details found." };
+//       })
+//       .catch((error) => {
+//         res.status(500).send(error.message || error);
+//       });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
-router.get("/logout", async (req, res, next) => {
-  try {
-    await auth().signOut();
-    res.send("User has successfully logged out.");
-  } catch (error) {
-    res.status(500).send(error.message || error);
-  }
-});
+// router.get("/logout", async (req, res, next) => {
+//   try {
+//     await auth().signOut();
+//     res.send("User has successfully logged out.");
+//   } catch (error) {
+//     res.status(500).send(error.message || error);
+//   }
+// });
 
 // router.get("/status", async (req, res, next) => {
 //   try {
